8197778df40aaa94dd6b11d87da1cbfc425da507
blueyaGIT <albanomarzia50@gmail.com>
2025-04-28 13:53:38 +0200

    added philo.h and main.c

---
+++ includes/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 13:52:11 by dalbano           #+#    #+#             */
+/*   Updated: 2025/04/28 13:52:12 by dalbano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+/* EXTERNAL INCLUDES */
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+/* INCLUDES */
+# include "libft/libft.h"
+
+# define TEST printf
+
+#endif /* PHILO_H */
