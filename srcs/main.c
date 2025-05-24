ce8452d06f033109b241aa4e016c4ecc34bcc27f
blueyaGIT <albanomarzia50@gmail.com>
2025-05-24 14:12:59 +0200

    relocated and updated main

---
+++ srcs/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalbano <dalbano@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 13:52:14 by dalbano           #+#    #+#             */
+/*   Updated: 2025/05/24 14:09:30 by dalbano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int ac, char **av)
+{
+	t_philo	**philos;
+	t_table	table;
+	t_data	data;
+
+	philos = NULL;
+	if (ac < 5 || ac > 6)
+		return (error_msg(USAGE), EXIT_FAILURE);
+	if (!handle_input(&data, av + 1) || !table_init(&table, &data))
+		return(EXIT_FAILURE);
+	philos = philos_init(&table);
+	(void)philos;
+	return (0);
+}
